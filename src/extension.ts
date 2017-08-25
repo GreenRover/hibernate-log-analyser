d163fe8772121e024c5b0da830906e0c2bf9b2d8
Heiko Henning <heiko.henning@mtrail.ch>
2017-08-25 08:11:28 +0200

    Sql Log will be highlighted

--- src/extension.ts
+++ src/extension.ts
@@ -3,7 +3,8 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import * as fs from 'fs';
-import { HibernateExtractorContentProvider } from './HibernateExtractorContentProvider';
+import { HibernateLogExtractor } from './HibernateLogExtractor';
+import { HibernateLogExtractorConfig } from './HibernateLogExtractor/config';
 import { FileReloadContentProvider } from './FileReloadContentProvider';
 
 
@@ -11,21 +12,11 @@ import { FileReloadContentProvider } from './FileReloadContentProvider';
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-    let previewSchema = 'sql-log';
+    const status = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
 
     let extractHibernateLogCommand = vscode.commands.registerCommand('hibernateLogExtract.extractHibernateLog', () => {
         return extractSqlFromHibernateLog();
     });
-    let hibernateExtractorProvider = new HibernateExtractorContentProvider();
-    vscode.window.onDidChangeTextEditorSelection((e: vscode.TextEditorSelectionChangeEvent) => {
-		if (e.textEditor === vscode.window.activeTextEditor) {
-			hibernateExtractorProvider.update(vscode.Uri.parse(previewSchema + "?" + vscode.window.activeTextEditor.document.uri.path));
-		}
-    })
-    
-    context.subscriptions.push(extractHibernateLogCommand, vscode.Disposable.from(
-        vscode.workspace.registerTextDocumentContentProvider(previewSchema, hibernateExtractorProvider)
-    ));
 
     let reloadFileCommand = vscode.commands.registerCommand('hibernateLogExtract.reloadFile', () => {
        return reloadFile();
@@ -44,55 +35,88 @@ export function activate(context: vscode.ExtensionContext) {
      context.subscriptions.push(clearFileCommand, vscode.Disposable.from(
          vscode.workspace.registerTextDocumentContentProvider('fileReload', fileReload)
      ));
-    
 
-    function extractSqlFromHibernateLog() {
+     function extractSqlFromHibernateLog() {
         var editor = vscode.window.activeTextEditor;
-        var path;
+        var path: string;
         if (editor) {
-            path = editor.document.uri.path;
+            path = editor.document.uri.fsPath;
         } else {
             console.log("No open text editor");
             
             if (vscode.workspace.textDocuments.length == 1) {
-                path = vscode.workspace.textDocuments[0].uri.path;
+                path = vscode.workspace.textDocuments[0].uri.fsPath;
             } else {
                 vscode.window.showErrorMessage("Please open any Text in Editor or check that log file is not to big");
                 return null;
             }
         }
-
-        return vscode.workspace.openTextDocument(vscode.Uri.parse(previewSchema + ":" + path)).then(doc => {
-            vscode.window.showTextDocument(doc, vscode.ViewColumn.One).then(editor => {
-                
-            });
+    
+        let stats: Map<string, number> = new Map();        
+    
+        let options: Object = {
+            content: getSQL(path, stats),
+            language: "sql"
+        };
+    
+        return vscode.workspace.openTextDocument(options).then(doc => {
+            vscode.window.showTextDocument(doc, vscode.ViewColumn.One);
+            updateStatus(stats);
         }, err => {
             vscode.window.showErrorMessage(err);
         });
     }
 
-    function clearFile() {
-        let editor = vscode.window.activeTextEditor;
-        fs.truncateSync(vscode.window.activeTextEditor.document.uri.fsPath);
-    }
-
-    function reloadFile() {
-        let editor = vscode.window.activeTextEditor;
-        let uri = encodeUri(vscode.window.activeTextEditor.document.uri);
-        return vscode.workspace.openTextDocument(uri).then(doc => {
-            vscode.commands.executeCommand('workbench.action.closeActiveEditor');
-            vscode.window.showTextDocument(doc, editor.viewColumn)
-        }, err => {
-            vscode.window.showErrorMessage(err);
+     function updateStatus(stats: Map<string, number>): void {
+        let statusStrings: Array<string> = [];
+        stats.forEach((value: number, key: string) => {
+            statusStrings.push(key + " " + value);
         });
-    }
-
-    function encodeUri(uri: vscode.Uri): vscode.Uri {
-        return vscode.Uri
-            .parse(`fileReload:${uri.path}`);
+    
+        status.text = statusStrings.join(", ");
+        status.show();
     }
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {
+}
+
+function getSQL(path: string, stats: Map<string, number>): string {
+    let text: string = fs.readFileSync(path, "utf8");
+    let logExtractor = new HibernateLogExtractor(getConfig());
+    let sql: string = logExtractor.extract(text, stats);
+
+    return sql;
+}
+
+function getConfig(): HibernateLogExtractorConfig {
+    let rawConfig: any = vscode.workspace.getConfiguration('hibernateLogExtract');
+    let config = new HibernateLogExtractorConfig();
+    config.hql = rawConfig.hql;
+    config.sqlComment = rawConfig.sqlComment;
+    config.statistic = rawConfig.statistic;
+
+    return config;
+}
+
+function clearFile() {
+    let editor = vscode.window.activeTextEditor;
+    fs.truncateSync(vscode.window.activeTextEditor.document.uri.fsPath);
+}
+
+function reloadFile() {
+    let editor = vscode.window.activeTextEditor;
+    let uri = encodeUri(vscode.window.activeTextEditor.document.uri);
+    return vscode.workspace.openTextDocument(uri).then(doc => {
+        vscode.commands.executeCommand('workbench.action.closeActiveEditor');
+        vscode.window.showTextDocument(doc, editor.viewColumn)
+    }, err => {
+        vscode.window.showErrorMessage(err);
+    });
+}
+
+function encodeUri(uri: vscode.Uri): vscode.Uri {
+    return vscode.Uri
+        .parse(`fileReload:${uri.path}`);
 }
